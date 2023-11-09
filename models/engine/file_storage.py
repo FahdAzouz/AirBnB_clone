46fbf19f66be7218aeaf155d407812b88c6cdfe3
FahdAzouz <fahdstudies00@gmail.com>
2023-11-09 12:39:58 +0100

    update storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,7 +35,7 @@ class FileStorage:
         with open(self.__file_path, mode="w") as f:
             dict_storage = {k: v.to_dict() for k, v in self.__objects.items()}
             json.dump(dict_storage, f)
-    
+
     def reload(self):
         """
         deserializes the JSON file to __objects
@@ -52,6 +52,57 @@ class FileStorage:
         """
         Returns a dictionary of valid classes and their references.
         """
-        
+        from models.place import Place
+        from models.review import Review
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.user import BaseModel
 
-    
+        array = {"BaseModel": BaseModel,
+                 "User": User,
+                 "State": State,
+                 "City": City,
+                 "Amenity": Amenity,
+                 "Place": Place,
+                 "Review": Review}
+        return array
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname."""
+        almutafa3i = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+                     {"place_id": str,
+                      "user_id": str,
+                      "text": str}
+                     }
+        return almutafa3i
