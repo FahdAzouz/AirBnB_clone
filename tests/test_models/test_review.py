30f9e595db04125ae41b08776c9eb68af1880d67
Fahd Azouz <104470633+FahdAzouz@users.noreply.github.com>
2023-11-11 21:03:41 +0100

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -2,15 +2,25 @@
 """
 test_review module
 """
-from unittest import TestCase
 import pycodestyle
 from models.review import Review
+from unittest import TestCase
 
 
 class TestReview(TestCase):
     """
     TestReview class
     """
+
+    def test_class_doc(self):
+        """test class documentation"""
+        doc = Review.__doc__
+        self.assertGreater(len(doc), 1)
+
+    def test_module_doc(self):
+        """test module documentation"""
+        doc = __import__('models.review').__doc__
+        self.assertGreater(len(doc), 1)
 
     def test_pep(self):
         """test pep"""
@@ -19,13 +29,3 @@ class TestReview(TestCase):
                                     'tests/test_models/test_review.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
-
-    def test_module_doc(self):
-        """test module documentation"""
-        doc = __import__('models.review').__doc__
-        self.assertGreater(len(doc), 1)
-
-    def test_class_doc(self):
-        """test class documentation"""
-        doc = Review.__doc__
-        self.assertGreater(len(doc), 1)
