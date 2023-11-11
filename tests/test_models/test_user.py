4e9f27ed8392812fe0b209b0ebd4dc11d5645c6d
Fahd Azouz <104470633+FahdAzouz@users.noreply.github.com>
2023-11-11 21:05:12 +0100

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -12,6 +12,11 @@ class TestUser(TestCase):
     TestUser class
     """
 
+    def test_module_doc(self):
+        """test module documentation"""
+        doc = __import__('models.user').__doc__
+        self.assertGreater(len(doc), 1)
+
     def test_pep(self):
         """test pep"""
         style = pycodestyle.StyleGuide(quiet=True)
@@ -19,11 +24,6 @@ class TestUser(TestCase):
                                     'tests/test_models/test_user.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
-
-    def test_module_doc(self):
-        """test module documentation"""
-        doc = __import__('models.user').__doc__
-        self.assertGreater(len(doc), 1)
 
     def test_class_doc(self):
         """test class documentation"""
