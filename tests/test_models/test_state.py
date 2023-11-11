0c6ea1d5d810ae9f92c3105d8d1c473968d9b4b7
Fahd Azouz <104470633+FahdAzouz@users.noreply.github.com>
2023-11-11 21:04:27 +0100

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -12,6 +12,11 @@ class TestState(TestCase):
     TestState class
     """
 
+    def test_module_doc(self):
+        """test module documentation"""
+        doc = __import__('models.state').__doc__
+        self.assertGreater(len(doc), 1)
+
     def test_pep(self):
         """test pep"""
         style = pycodestyle.StyleGuide(quiet=True)
@@ -19,11 +24,6 @@ class TestState(TestCase):
                                     'tests/test_models/test_state.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
-
-    def test_module_doc(self):
-        """test module documentation"""
-        doc = __import__('models.state').__doc__
-        self.assertGreater(len(doc), 1)
 
     def test_class_doc(self):
         """test class documentation"""
