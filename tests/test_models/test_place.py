02cbff62f11507c2b4b5e46c05e8b3e0d3847045
Fahd Azouz <104470633+FahdAzouz@users.noreply.github.com>
2023-11-11 21:01:46 +0100

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -2,15 +2,25 @@
 """
 test_place module
 """
-from unittest import TestCase
 import pycodestyle
 from models.place import Place
+from unittest import TestCase
 
 
 class TestPlace(TestCase):
     """
     TestPlace class
     """
+
+    def test_class_doc(self):
+        """test class documentation"""
+        doc = Place.__doc__
+        self.assertGreater(len(doc), 1)
+
+    def test_module_doc(self):
+        """test module documentation"""
+        doc = __import__('models.place').__doc__
+        self.assertGreater(len(doc), 1)
 
     def test_pep(self):
         """test pep"""
@@ -19,13 +29,3 @@ class TestPlace(TestCase):
                                     'tests/test_models/test_place.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
-
-    def test_module_doc(self):
-        """test module documentation"""
-        doc = __import__('models.place').__doc__
-        self.assertGreater(len(doc), 1)
-
-    def test_class_doc(self):
-        """test class documentation"""
-        doc = Place.__doc__
-        self.assertGreater(len(doc), 1)
