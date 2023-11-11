74b42d4929c497ec5003a0f228ca5f959e8f4448
Fahd Azouz <104470633+FahdAzouz@users.noreply.github.com>
2023-11-11 21:00:20 +0100

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -2,15 +2,25 @@
 """
 test_city module
 """
-from unittest import TestCase
 import pycodestyle
 from models.city import City
+from unittest import TestCase
 
 
 class TestCity(TestCase):
     """
     TestCity class
     """
+
+    def test_class_doc(self):
+        """test class documentation"""
+        doc = City.__doc__
+        self.assertGreater(len(doc), 1)
+
+    def test_module_doc(self):
+        """test module documentation"""
+        doc = __import__('models.city').__doc__
+        self.assertGreater(len(doc), 1)
 
     def test_pep(self):
         """test pep"""
@@ -19,13 +29,3 @@ class TestCity(TestCase):
                                     'tests/test_models/test_city.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
-
-    def test_module_doc(self):
-        """test module documentation"""
-        doc = __import__('models.city').__doc__
-        self.assertGreater(len(doc), 1)
-
-    def test_class_doc(self):
-        """test class documentation"""
-        doc = City.__doc__
-        self.assertGreater(len(doc), 1)
