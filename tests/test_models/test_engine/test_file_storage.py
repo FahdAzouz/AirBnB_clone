800ce8136be1ba032f8e68d9d60ab65764d5461b
Fahd Azouz <104470633+FahdAzouz@users.noreply.github.com>
2023-11-11 21:06:10 +0100

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,15 +2,20 @@
 """
 test_amenity module
 """
-from unittest import TestCase
-import pycodestyle
 from models.engine.file_storage import FileStorage
+import pycodestyle
+from unittest import TestCase
 
 
 class TestFileStorage(TestCase):
     """
     TestFileStorage class
     """
+
+    def test_module_doc(self):
+        """test module documentation"""
+        doc = __import__('models.engine.file_storage').__doc__
+        self.assertGreater(len(doc), 1)
 
     def test_pep(self):
         """test pep"""
@@ -20,11 +25,6 @@ class TestFileStorage(TestCase):
         result = style.check_files(files)
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
-
-    def test_module_doc(self):
-        """test module documentation"""
-        doc = __import__('models.engine.file_storage').__doc__
-        self.assertGreater(len(doc), 1)
 
     def test_class_doc(self):
         """test class documentation"""
