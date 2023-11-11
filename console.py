c0692c6f06281bcdcbb2e6d48ddf5c36359d1ae9
Fahd Azouz <104470633+FahdAzouz@users.noreply.github.com>
2023-11-11 18:29:08 +0100

    Update console.py

--- console.py
+++ console.py
@@ -2,10 +2,10 @@
 """
 console module
 """
-import cmd
 from models.base_model import BaseModel
-from models import storage
+import cmd
 import re
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -14,10 +14,6 @@ class HBNBCommand(cmd.Cmd):
     """
 
     prompt = "(hbnb) "
-
-    def custom_all(self, match):
-        """Custom all() commander."""
-        print("ALL()", match)
 
     def precmd(self, line):
         """Intercepts commands to test for class.syntax()"""
@@ -31,29 +27,22 @@ class HBNBCommand(cmd.Cmd):
         self.onecmd(command)
         return ""
 
-    def do_EOF(self, line):
-        """handles EOF"""
-        return True
-
     def do_quit(self, line):
         """Quit command to exit the program
         """
         return True
 
+    def custom_all(self, match):
+        """Custom all() commander."""
+        print("ALL()", match)
+
+    def do_EOF(self, line):
+        """handles EOF"""
+        return True
+
     def emptyline(self):
         """an empty line"""
         pass
-
-    def do_create(self, line):
-        """Creates a new instance of BaseModel"""
-        if line == "" or line is None:
-            print("** class name missing **")
-        elif line not in storage.classes():
-            print("** class doesn't exist **")
-        else:
-            obj = storage.classes()[line]()
-            obj.save()
-            print(obj.id)
 
     def do_show(self, line):
         """Prints the string representation of an
@@ -73,6 +62,17 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     print(storage.all()[key])
 
+    def do_create(self, line):
+        """Creates a new instance of BaseModel"""
+        if line == "" or line is None:
+            print("** class name missing **")
+        elif line not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            obj = storage.classes()[line]()
+            obj.save()
+            print(obj.id)
+
     def do_destroy(self, line):
         """Deletes an instance based on the class name and id"""
         if line == "" or line is None:
@@ -91,6 +91,17 @@ class HBNBCommand(cmd.Cmd):
                     del storage.all()[key]
                     storage.save()
 
+    def do_count(self, line):
+        """retrieve the number of instances of a class"""
+        args = line.split(' ')
+        if not args[0]:
+            print("** class name missing **")
+        elif args[0] not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            matches = [k for k in storage.all() if k.startswith(args[0] + '.')]
+            print(len(matches))
+
     def do_all(self, line):
         """Prints all string representation of all
         instances based or not on the class name"""
@@ -105,17 +116,6 @@ class HBNBCommand(cmd.Cmd):
         else:
             l = [str(obj) for key, obj in storage.all().items()]
             print(l)
-
-    def do_count(self, line):
-        """retrieve the number of instances of a class"""
-        args = line.split(' ')
-        if not args[0]:
-            print("** class name missing **")
-        elif args[0] not in storage.classes():
-            print("** class doesn't exist **")
-        else:
-            matches = [k for k in storage.all() if k.startswith(args[0] + '.')]
-            print(len(matches))
 
     def do_update(self, line):
         """Updates an instance based on the class\
